7580ff801e62f6f7bbf638e1158c0702b472e062
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-03-09 18:15:24 +0530

    Update maximum_sum_subarray.py

--- Dynamic/maximum_sum_subarray.py
+++ Dynamic/maximum_sum_subarray.py
@@ -15,3 +15,32 @@ def lis(arr):
 a = [1,2,3,4,5,-20,1,2,5,7,9]
 m = lis(a)
 print(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# M-2
+arr  = [1,2,-5,6,7,-10,5,6,7,8]
+# arr= [1,2,-5,1,0]
+sum_till_now, max_sum = 0,0
+for i in range(len(arr)):
+    if sum_till_now+arr[i]>0:
+        sum_till_now = sum_till_now+arr[i]
+        if sum_till_now>max_sum:
+            max_sum=sum_till_now
+    elif sum_till_now+arr[i]<0:
+        sum_till_now = 0
+print(max_sum)
