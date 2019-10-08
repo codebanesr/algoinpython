b56f86b475e7f55555cf8be806477606a80e9fe7
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-10-09 00:38:47 +0530

    Adding description for stack sortable

--- scalable-graph-algorithms/Check if a given array can represent Preorder Traversal of Binary Search Tree.py
+++ scalable-graph-algorithms/Check if a given array can represent Preorder Traversal of Binary Search Tree.py
@@ -14,3 +14,17 @@ def isbstpreorder(iterable):
     
     
 https://stackoverflow.com/questions/26173808/checking-if-given-preorder-traversal-is-valid-bst
+    
+    
+    
+    
+#     How to check for 231 pattern
+mark first node as root, wait for the next greater node and mark it as root,
+
+if a node greater than this arrives we have reached our first partition
+
+lets mark it as the root, now after this we can have element greater or smaller than the root
+if we get an element greater than this we keep that element in the stack, remember we havent changed the root but what we have
+is the root of the RST and remember for RST we can never have an element greater than the root. If it happens just return false;
+everytime you get an element greater than the element on stack we are moving furthur right and can never have an element less
+than the root....... That ends the discussion not completely
